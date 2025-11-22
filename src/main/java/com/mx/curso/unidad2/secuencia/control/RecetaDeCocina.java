741ee6292e3e79f4e7a6ecdfaed9ab77130d255e
Carloshzm <ux24nt278@ux.edu.mx>
2025-11-22 08:08:27 -0600

    Se suben las actividades de la unidad 2, día tres

---
+++ src/main/java/com/mx/curso/unidad2/secuencia/control/RecetaDeCocina.java
@@ -0,0 +1,20 @@
+package com.mx.curso.unidad2.secuencia.control;
+
+public class RecetaDeCocina {
+
+    static void main(String[] args) {
+        int ingredientesPreparados = 0;
+        int tipoDeCocina = 0;
+
+        System.out.println("Paso 1: Preparar los ingredientes");
+        System.out.println(ingredientesPreparados++);
+        System.out.println("Agregar ingrediente al platillo");
+        System.out.println(ingredientesPreparados++);
+
+        System.out.println("Paso 2: Cocina platillo");
+        System.out.println(tipoDeCocina++);
+
+        System.out.println("Paso 3: Servir el platillo");
+
+    }
+}
---
+++ src/main/java/com/mx/curso/unidad2/secuencia/sentencias/OperacionMatematica.java
@@ -0,0 +1,13 @@
+package com.mx.curso.unidad2.secuencia.sentencias;
+
+public class OperacionMatematica {
+
+    static void main(String[] args) {
+        int producto = 8*2;
+        int cociente = 10/2;
+
+        System.out.println("El resultado de producto es: " + producto);
+        System.out.println("El resultado de producto es: " + cociente);
+
+    }
+}
